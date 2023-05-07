05ff6ff0fa562ff37522984edd408d016b59b007
pphila <efl2ice89@gmail.com>
2023-05-07 16:38:50 -0700

    add construction splash

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <section>
-        <Navbar />
+        {/* <Navbar /> */}
+        <div className='flex justify-center'>
+          <Image src="/../public/assets/img/MM.png" alt="/" width="250" height="250"/>
+        </div>
+        
       </section>
 
       <main>
-      <h1 className="text-6xl text-center py-40">MATSU | <span className='text-[#E74646] font-semibold'>dot</span> | MOTO</h1>
-        <p className="text-center">-Under Construction-</p>
+        <p className='uppercase text-sm text-center tracking-widest text-gray-600'>PDX House DJ</p>
+        <h1 className="text-6xl text-center py-40">MATSU | <span className='text-[#E74646] font-semibold'>dot</span> | MOTO</h1>
+        <h2 className="text-center">-Under Construction-</h2>
       </main>
 
       {/* <section>
