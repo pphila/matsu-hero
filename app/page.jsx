14dee5285b979648fdf493de6b22f84ff32d6766
pphila <efl2ice89@gmail.com>
2023-05-08 17:11:45 -0700

    WIP for image routing bug

--- app/page.jsx
+++ app/page.jsx
@@ -9,14 +9,14 @@ export default function Home() {
       <section>
         {/* <Navbar /> */}
         <div className='flex justify-center'>
-          <Image src="/../public/assets/img/MM.png" alt="/" width="250" height="250"/>
+          <Image src="/../app/img/matsumoto-logo.png" alt="/" width="250" height="250"/>
         </div>
         
       </section>
 
       <main>
-        <p className='uppercase text-sm text-center tracking-widest text-gray-600'>PDX House DJ</p>
-        <h1 className="text-6xl text-center py-40">MATSU | <span className='text-[#E74646] font-semibold'>dot</span> | MOTO</h1>
+        <p className='uppercase text-sm text-center py-20 tracking-widest text-gray-600'>PDX House DJ</p>
+        <h1 className="text-6xl text-center py-20">MATSU | <span className='text-[#E74646] font-semibold'>dot</span> | MOTO</h1>
         <h2 className="text-center">-Loading CDJs-</h2>
       </main>
 
