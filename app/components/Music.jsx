cc92caca6e2d1444a92869bd797ebcc16492ffaf
pphila <efl2ice89@gmail.com>
2023-05-09 20:50:53 -0700

    add content to music section and implemented soundcloud widget :sparkles:

--- app/components/Music.jsx
+++ app/components/Music.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Music = () => {
   return (
     <>
-      <div className='w-full h-screen'>
-        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-          <div className='py-4'>
-            <h1 className='text-6xl text-left'>Music</h1>
-            <p>SoundCloud Imbed here</p>
+      <div className='w-full md:h-screen p-2 py-16'>
+        <h1 className='max-w-[1240px] m-auto text-6xl text-left pb-2'>Music</h1>
+        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+          <div className='col-span-2'>
+            <div className='w-full h-auto m-auto shadow-2xl shadow-gray-400 flex items-center justify-center'>
+              <Image className='rounded-sm' src="/../public/assets/img/matsu-music.jpg" width="828" height="552" alt="bio pic" />
+            </div>
+          </div>
+          <div className='pt-[60]'>
+            <iframe width="100%" height="544" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/matsudotmoto&amp;"></iframe>
           </div>
         </div>
       </div>
