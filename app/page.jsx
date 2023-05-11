41105b675bcdf04c2f318509d3d017b91d72c19d
pphila <efl2ice89@gmail.com>
2023-05-10 19:33:04 -0700

    add working background img

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,8 @@ import Music from './components/Music';
 import Photos from './components/Photos';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-
 // import Image from 'next/image';
+// import bgImg from './../public/assets/img/matsu-bg.jpg';
 
 export default function Home() {
   return (
@@ -22,7 +22,10 @@ export default function Home() {
       </section>
 
       <main>
-
+        
+        <div className='flex items-center justify-center fixed bg-cover bg-center bg-no-repeat w-full h-screen custom-image z-[-1]'>
+          {/* Container for background image */}
+        </div>
         {/* <p className='uppercase text-sm text-center tracking-widest text-gray-600'>PDX House DJ</p>
         <h1 className="text-6xl text-center py-40">MATSU | <span className='text-[#E74646] font-semibold'>dot</span> | MOTO</h1>
         <h2 className="text-center">-Under Construction-</h2> */}
