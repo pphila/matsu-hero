ac8ed9f4067014929e7de41e1369266c8a30cf60
pphila <efl2ice89@gmail.com>
2023-05-10 20:01:55 -0700

    removed commented sections from page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,28 +7,19 @@ import Photos from './components/Photos';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 // import Image from 'next/image';
-// import bgImg from './../public/assets/img/matsu-bg.jpg';
 
 export default function Home() {
   return (
     <>
       <section>
         <Navbar />
-        
-        {/* <div className='flex justify-center'>
-          <Image src="/../public/assets/img/MM.png" alt="/" width="250" height="250"/>
-        </div> */}
-        
       </section>
 
       <main>
         
-        <div className='flex items-center justify-center fixed bg-cover bg-center bg-no-repeat w-full h-screen custom-image z-[-1]'>
+        <div className='fixed bg-cover bg-center bg-no-repeat w-full h-screen custom-image z-[-1]'>
           {/* Container for background image */}
         </div>
-        {/* <p className='uppercase text-sm text-center tracking-widest text-gray-600'>PDX House DJ</p>
-        <h1 className="text-6xl text-center py-40">MATSU | <span className='text-[#E74646] font-semibold'>dot</span> | MOTO</h1>
-        <h2 className="text-center">-Under Construction-</h2> */}
 
         <section>
           <Hero />
