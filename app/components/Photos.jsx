fc5011a52bb6891d85ccb6dc81ca317311ef61ca
pphila <efl2ice89@gmail.com>
2023-05-10 21:24:39 -0700

    add logic for a photo gallery in photo section with locally stored images added to assets

--- app/components/Photos.jsx
+++ app/components/Photos.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import Image from 'next/image';
+import Img1 from './../../public/assets/img/photoGallery/matsu-img-1.jpg';
+import Img2 from './../../public/assets/img/photoGallery/matsu-img-2.jpg';
+import Img3 from './../../public/assets/img/photoGallery/matsu-img-3.jpg';
+import Img4 from './../../public/assets/img/photoGallery/matsu-img-4.jpg';
+import Img5 from './../../public/assets/img/photoGallery/matsu-img-5.jpg';
+import Img6 from './../../public/assets/img/photoGallery/matsu-img-6.jpg';
+
 
 const PhotoReel = () => {
   return(
     <>
-      <div className='w-full h-screen text-center'>
-        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-          <div className='py-4'>
-            <h1 className="text-6xl text-center">Photos</h1>
+      <div className='bg-gray-50 w-full h-full text-center pb-10'>
+        <div className='py-4'>
+            <h1 className="text-6xl text-gray-950 text-center py-6">Photos</h1>
+        </div>
+        <div className='max-w-[1240px] mx-auto md:grid grid-cols-3 gap-6 pt-10'>
+          
+          <div className='w-fit m-auto shadow-lg shadow-gray-900 flex items-center justify-center hover:scale-110 ease-in duration-200'>
+            <Image src={Img1} width="400" height="250" alt='/'/>
           </div>
+          <div className='w-fit m-auto shadow-lg shadow-gray-900 flex items-center justify-center hover:scale-110 ease-in duration-200'>
+            <Image src={Img2} width="400" height="250" alt='/'/>
+          </div>
+          <div className='w-fit m-auto shadow-lg shadow-gray-900 flex items-center justify-center hover:scale-110 ease-in duration-200'>
+            <Image src={Img3} width="400" height="250" alt='/'/>
+          </div>
+          <div className='w-fit m-auto shadow-lg shadow-gray-900 flex items-center justify-center hover:scale-110 ease-in duration-200'>
+            <Image src={Img4} width="400" height="250" alt='/'/>
+          </div>
+          <div className='w-fit m-auto shadow-lg shadow-gray-900 flex items-center justify-center hover:scale-110 ease-in duration-200'>
+            <Image src={Img5} width="400" height="250" alt='/'/>
+          </div>
+          <div className='w-fit m-auto shadow-lg shadow-gray-900 flex items-center justify-center hover:scale-110 ease-in duration-200'>
+            <Image src={Img6} width="400" height="250" alt='/'/>
+          </div>
+
         </div>
       </div>
     </>
