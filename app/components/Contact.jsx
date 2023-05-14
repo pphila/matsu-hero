f708cc9f36970dd71b16a63a4599658ce06531a8
pphila <efl2ice89@gmail.com>
2023-05-14 12:42:05 -0700

    :construcrion: WIP for contact section

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Contact = () => {
   return(
     <>
-      <div className='w-full h-screen text-center'>
-        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+      <div className='w-full lg:h-screen text-center'>
+        <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+          
           <div className='py-4'>
             <h1 className="text-6xl text-center">Contact Me:</h1>
-            <p>Contact Form goes here:</p>
+            <p>Contact me for availibility on your next event!</p>
           </div>
+
+          <div className='grid lg:grid-cols-5 gap-8'>
+            <div className='lg:p-4 h-full'>
+              <div>
+                <Image />
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </>
