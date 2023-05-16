03a6a8c2db23e13c008295e0518329c77bd85740
pphila <efl2ice89@gmail.com>
2023-05-15 20:18:23 -0700

    add more content for bio section, changed background to white non transparent, fixing margins and padding of elements

--- app/components/Bio.jsx
+++ app/components/Bio.jsx
@@ -5,14 +5,18 @@ import bioPic from './../../public/assets/img/matsu.MOTO-bio.jpg';
 const Bio = () => {
   return (
     <>
-      <div id='bio' className='bg-gray-50/20 text-gray-50 w-full md:h-screen p-2 py-16'>
-        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-2'>
+      <div id='bio' className='bg-gray-50 text-gray-950 w-full md:h-screen p-2 py-16'>
+        <div className='max-w-[1240px] m-auto md:grid grid-cols-2 gap-2'>
           <div className='col-span-2 grid justify-center align-middle mt-16'>
             <h1 className='text-6xl text-center'>Biography</h1>
-            <p className='py-2 font-semibold'>I am DJ Matsu.MOTO, Portland based artist originally from Honolulu, Hawaii. I am an up an coming DJ playing at local venues and a residency at Portland's premiere EDM club 45 East. Opening for big names such as Valentino Kahn and Audien. Mixing big room bass & booty shaking house. lore</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque voluptatum quos inventore doloribus reprehenderit esse sunt laborum magni, molestiae vel sequi nihil numquam porro, ipsam rem asperiores, repellendus ea eos!</p>
+            <div className='max-w-[800] bg-gray-50/25 p-4 mt-4'>
+              <p className='py-2 text-lg font-semibold mt-4'>DJ Matsu.MOTO is a Honolulu-born, Portland-based DJ working his way to the the big stage. Inspired by big name artists like Nitti Gritti, John Summit, and artist like Malaa, he has honed his skills and developed a unique sound that blends House, Tech House, and Big Room Bass.</p>
+              <p className='py-2 text-lg font-semibold'>With a residency at Portland's top EDM club 45 East, Matsu.MOTO has become a top rising DJ in the Portland area. Opening for a wide range of artists like Keys n Krate, S!ckick, Madeon, Capozzi, Nitti Gritti, Boombox Cartel, SNBRN, Dillon Francis, and DJ Pauly D. Showcasing versatility and ability to cater to different crowds.</p>
+              <p className='py-2 text-lg font-semibold'>Matsu.MOTO sets are characterized by infectious beats, booty shaking bass, and a keen eye for what the crowd wants. Whether playing for thousands at a club or a for a smaller crowd in more intimate venues, Matsu.MOTO never fails to keep the energy high and the dance floor moving.</p>
+              <p className='py-2 text-lg font-semibold'>With his passion for music, the EDM culture, and the dedication to his sets, Matsu.MOTO will keep you bouncing, shuffling, fan clacking, and even woot-wooting on the dance floor.</p>
+            </div>
           </div>
-          <div className='w-full h-auto m-auto shadow-2xl col-span-2 shadow-gray-950 flex items-center justify-center hover:scale-110 ease-in duration-200'>
+          <div className='w-fit h-auto m-auto shadow-2xl col-span-2 shadow-gray-950 flex items-center justify-center'>
             <Image src={bioPic} width="828" height="552" alt="bio pic" />
           </div>
         </div>
